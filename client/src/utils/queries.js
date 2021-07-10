00b54a6d70a5ab14b834c38e3b29b5d62ab96654
Savannah Bennett <savvy.bennett8@gmail.com>
2021-07-10 15:30:20 -0600

    fixed me query to return saved books properly

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,19 +1,21 @@
 import { gql } from 'graphql-tag';
 
 export const GET_ME = gql`
-    query me {
-        _id
-        username
-        email
-        bookCount
-        savedBooks {
-            # _id
-            bookId
-            authors
-            description
-            title
-            image
-            link
+    {
+        me {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                # _id
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
         }
     }
 `;
